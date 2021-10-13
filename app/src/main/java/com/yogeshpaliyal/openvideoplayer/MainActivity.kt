09c55f8a84b3cfa837ee68d675209039d03aa498
Yogesh Choudhary Paliyal <paliyalyogesh@gmail.com>
2021-10-13 22:45:37 +0530

    WIP init viewmodel

--- app/src/main/java/com/yogeshpaliyal/openvideoplayer/MainActivity.kt
+++ app/src/main/java/com/yogeshpaliyal/openvideoplayer/MainActivity.kt
@@ -3,21 +3,31 @@ package com.yogeshpaliyal.openvideoplayer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.yogeshpaliyal.openvideoplayer.ui.list.ListViewModel
+import com.yogeshpaliyal.openvideoplayer.ui.list.VideosList
 import com.yogeshpaliyal.openvideoplayer.ui.theme.OpenVideoPlayerTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel by viewModels<ListViewModel>()
+
+    @OptIn(ExperimentalPermissionsApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewModel
         setContent {
             OpenVideoPlayerTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    //VideosList()
                 }
             }
         }
--- app/src/main/java/com/yogeshpaliyal/openvideoplayer/ui/list/VideosList.kt
+++ app/src/main/java/com/yogeshpaliyal/openvideoplayer/ui/list/VideosList.kt
@@ -1,23 +1,62 @@
 package com.yogeshpaliyal.openvideoplayer.ui.list
 
 import android.net.Uri
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.PermissionRequired
+import com.google.accompanist.permissions.rememberPermissionState
 import com.yogeshpaliyal.openvideoplayer.data.Video
 
+@ExperimentalPermissionsApi
 @Composable
-fun VideosList(viewModel: ListViewModel = viewModel()) {
-    val videos = viewModel.getVideos().collectAsState(initial = listOf())
+fun VideosList(viewModel: ListViewModel = hiltViewModel()) {
 
-    videos.value.let {
-        VideosListData(it)
+    val readStoragePermissionState = rememberPermissionState(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+
+
+    PermissionRequired(
+        permissionState = readStoragePermissionState,
+        permissionNotGrantedContent = {
+
+            Column(modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()) {
+                Text(text = "Permission not granted")
+            }
+            readStoragePermissionState.run {
+                launchPermissionRequest()
+            }
+        },
+        permissionNotAvailableContent = {
+            Column(modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()) {
+                Text(text = "Permission not available")
+            }
+        }) {
+        val videos = viewModel.getVideos().collectAsState(initial = listOf())
+
+        videos.value.let {
+            VideosListData(it)
+        }
     }
+
+
+
+
 }
 
 
@@ -25,16 +64,22 @@ fun VideosList(viewModel: ListViewModel = viewModel()) {
 fun VideosListData(videos: List<Video>) {
     LazyColumn {
         itemsIndexed(videos) { index: Int, item: Video ->
-            Text(text = item.videoName)
-            if (index != videos.lastIndex)
-                Divider()
+            Column {
+                Text(modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp), text = item.videoName)
+                if (index != videos.lastIndex)
+                    Divider()
+
+            }
         }
     }
 }
 
 @Preview(showSystemUi = true)
 @Composable
-fun PreviewVideoListData(){
-    val videos = listOf<Video>(Video(Uri.EMPTY, "Test Name",0,0))
+fun PreviewVideoListData() {
+    val videos =
+        listOf<Video>(Video(Uri.EMPTY, "Test Name", 0, 0), Video(Uri.EMPTY, "Test Name", 0, 0))
     VideosListData(videos = videos)
 }
