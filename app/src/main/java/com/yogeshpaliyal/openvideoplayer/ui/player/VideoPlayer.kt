2a41207d316b6d9f7b3e73fc91f7240905240a23
Yogesh Choudhary Paliyal <paliyalyogesh@gmail.com>
2021-10-19 21:50:42 +0530

    Working on controller

--- app/src/main/java/com/yogeshpaliyal/openvideoplayer/ui/player/VideoPlayer.kt
+++ app/src/main/java/com/yogeshpaliyal/openvideoplayer/ui/player/VideoPlayer.kt
@@ -1,11 +1,11 @@
 package com.yogeshpaliyal.openvideoplayer.ui.player
 
 import android.net.Uri
+import android.util.Log
 import android.view.View
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
@@ -15,15 +15,30 @@ import com.google.android.exoplayer2.source.ProgressiveMediaSource
 import com.google.android.exoplayer2.ui.PlayerView
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.util.Util
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
+
+const val TAG = "VideoPlayerTag"
 
 @Composable
 fun VideoDetail(videoUri: Uri) {
     // This is the official way to access current context from Composable functions
     val context = LocalContext.current
 
+    var controllerState by remember {
+        mutableStateOf(View.VISIBLE)
+    }
+
+    var progress by remember {
+        mutableStateOf(0)
+    }
+
+    Log.d(TAG, "VideoDetail: composed")
+
     // Do not recreate the player everytime this Composable commits
     val exoPlayer = remember(context) {
+        Log.d(TAG, "VideoDetail: create exoplayer")
         SimpleExoPlayer.Builder(context).build().apply {
             val dataSourceFactory = DefaultDataSourceFactory(
                 context,
@@ -33,7 +48,6 @@ fun VideoDetail(videoUri: Uri) {
             val source = ProgressiveMediaSource.Factory(dataSourceFactory)
                 .createMediaSource(
                     MediaItem.fromUri(
-                        // Big Buck Bunny from Blender Project
                         videoUri
                     )
                 )
@@ -43,19 +57,39 @@ fun VideoDetail(videoUri: Uri) {
     }
 
 
-    AndroidView(modifier = Modifier.fillMaxSize(), factory = { viewContext ->
-        PlayerView(viewContext).apply {
-            player = exoPlayer
-            exoPlayer.playWhenReady = true
-            setControllerVisibilityListener { visibility ->
-                if(visibility == View.VISIBLE){
-                    // show buttons
-                }else{
-                    // hide buttons
+        AndroidView(modifier = Modifier.fillMaxSize(), factory = { viewContext ->
+            PlayerView(viewContext).apply {
+                Log.d(TAG, "VideoDetail: PlayerView factory")
+                player = exoPlayer
+                exoPlayer.playWhenReady = true
+                useController = false
+                setOnClickListener {
+                    if (it.visibility == View.VISIBLE) {
+                        // hide buttons
+                        controllerState = View.GONE
+                        Log.d(TAG, "VideoDetail: visibility change gone")
+                    } else {
+                        // show buttons
+                        Log.d(TAG, "VideoDetail: visibility change visible")
+                        controllerState = View.VISIBLE
+                    }
                 }
+                exoPlayer.prepare()
             }
-            exoPlayer.prepare()
-        }
-    })
+        })
 
+    Controller(controllerState = controllerState)
+}
+
+
+@Composable
+fun Controller(controllerState : Int){
+    Log.d(TAG, "Controller: composed $controllerState")
+    if(controllerState == View.VISIBLE){
+        // visible
+        Text(text = "Contolls Visible")
+    }else{
+        // hide controller
+        Text(text = "Contolls Gone")
+    }
 }
