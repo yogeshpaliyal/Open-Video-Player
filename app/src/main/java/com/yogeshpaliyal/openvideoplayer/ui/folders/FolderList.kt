168d0d01ca7204fe83c38b6ee531392181f42c47
Yogesh Choudhary Paliyal <paliyalyogesh@gmail.com>
2021-10-20 21:12:01 +0530

    Understanding about states

--- app/src/main/java/com/yogeshpaliyal/openvideoplayer/ui/folders/FolderList.kt
+++ app/src/main/java/com/yogeshpaliyal/openvideoplayer/ui/folders/FolderList.kt
@@ -2,7 +2,6 @@ package com.yogeshpaliyal.openvideoplayer.ui.folders
 
 import android.Manifest
 import android.util.Log
-import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -16,10 +15,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavController
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.PermissionRequired
 import com.google.accompanist.permissions.rememberPermissionState
@@ -28,7 +25,7 @@ import com.yogeshpaliyal.openvideoplayer.ui.list.ListViewModel
 
 @ExperimentalPermissionsApi
 @Composable
-fun FolderList(navigateToVideos : (folder: Folder)-> Unit) {
+fun FolderList(navigateToVideos: (folder: Folder) -> Unit) {
 
     val readStoragePermissionState =
         rememberPermissionState(Manifest.permission.READ_EXTERNAL_STORAGE)
@@ -83,10 +80,11 @@ fun FolderListData(
     LazyColumn {
         itemsIndexed(folders.value) { index: Int, item: Folder ->
             Row(modifier = Modifier
-                .padding(8.dp)
                 .clickable {
                     navigateToVideos(item)
-                }) {
+                }
+                .padding(8.dp)
+            ) {
 
                 Icon(Icons.Rounded.List, "", modifier = Modifier.align(Alignment.CenterVertically))
                 Spacer(modifier = Modifier.width(16.dp))
--- app/src/main/java/com/yogeshpaliyal/openvideoplayer/ui/player/VideoPlayer.kt
+++ app/src/main/java/com/yogeshpaliyal/openvideoplayer/ui/player/VideoPlayer.kt
@@ -3,20 +3,24 @@ package com.yogeshpaliyal.openvideoplayer.ui.player
 import android.net.Uri
 import android.util.Log
 import android.view.View
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.source.ProgressiveMediaSource
 import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.ui.StyledPlayerView
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.util.Util
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 
 const val TAG = "VideoPlayerTag"
@@ -53,42 +57,57 @@ fun VideoDetail(videoUri: Uri) {
                 )
 
             this.setMediaSource(source)
+            this.prepare()
         }
     }
 
 
-        AndroidView(modifier = Modifier.fillMaxSize(), factory = { viewContext ->
-            PlayerView(viewContext).apply {
-                Log.d(TAG, "VideoDetail: PlayerView factory")
-                player = exoPlayer
-                exoPlayer.playWhenReady = true
-                useController = false
-                setOnClickListener {
-                    if (it.visibility == View.VISIBLE) {
-                        // hide buttons
-                        controllerState = View.GONE
-                        Log.d(TAG, "VideoDetail: visibility change gone")
-                    } else {
-                        // show buttons
-                        Log.d(TAG, "VideoDetail: visibility change visible")
-                        controllerState = View.VISIBLE
-                    }
-                }
-                exoPlayer.prepare()
-            }
-        })
+    Player(exoPlayer)
 
-    Controller(controllerState = controllerState)
+    /*Controller(controllerState = controllerState) {
+        controllerState = it
+    }*/
+}
+
+@Composable
+fun Player(exoPlayer: SimpleExoPlayer) {
+    Log.d(TAG, "Player: Compose")
+
+    AndroidView(modifier = Modifier.fillMaxSize(), factory = { viewContext ->
+        StyledPlayerView(viewContext).apply {
+            Log.d(TAG, "Player: PlayerView factory")
+            player = exoPlayer
+            useController = false
+            setControllerVisibilityListener {
+                Log.d(TAG, "Player: Controller visibility change")
+            }
+            exoPlayer.playWhenReady = true
+        }
+    }, update = {
+        Log.d(TAG, "Player: PlayerView update")
+    })
 }
 
 
 @Composable
-fun Controller(controllerState : Int){
+fun Controller(controllerState: Int, changeControllerState: (Int) -> Unit) {
     Log.d(TAG, "Controller: composed $controllerState")
-    if(controllerState == View.VISIBLE){
+
+    val backgroundColor by remember(controllerState) {
+        mutableStateOf(if(controllerState == View.VISIBLE) Color(0x80000000) else Color(0x00000000))
+    }
+
+
+    Box(modifier = Modifier.fillMaxSize().background(backgroundColor).clickable {
+        changeControllerState(if (controllerState == View.VISIBLE) View.GONE else View.VISIBLE)
+    }) {
+
+    }
+
+    if (controllerState == View.VISIBLE) {
         // visible
         Text(text = "Contolls Visible")
-    }else{
+    } else {
         // hide controller
         Text(text = "Contolls Gone")
     }
